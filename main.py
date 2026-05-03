f6f6c4126d1bed420cf935f8285a51eba357fd80
Siverty_Home <justinschots13@gmail.com>
2026-05-03 15:59:19 +0200

    optimisation + documentation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+# Import apiprovider and worker
 from fastapi import FastAPI
 import uvicorn
+
+# Import routers
 from simple_createst.touch import router
-                                                                                                                            
+
+# Define API-name && routers                                                                                                         
 api = FastAPI()
 api.include_router(router)
--- simple_createst/touch.py
+++ simple_createst/touch.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 import subprocess as sub
 import os
 
@@ -6,20 +6,20 @@ import os
 # the endpoints there
 router = APIRouter()
 
+# Init run variable
 run = 0
 
+# Relative path finder
 file_dir = os.path.dirname(os.path.abspath(__file__))
 
-@router.get("/")
-def home():
+@router.get("/touch/{version}")
+def touch(version: int):
+    # Small optimisation to the file-call logic
+    if version not in (0, 1):
+        raise HTTPException(status_code=400, detail="version must be 0 or 1")
+    # Get globally defined variable
     global run
-    run = run + 1
-    sub.call(['sh', file_dir + '/touch_0.sh', str(run), str(file_dir)])
-    return {"Run": run}
-
-@router.get("/1")
-def home_1():
-    global run
-    run = run + 1
-    sub.call(['sh', file_dir + '/touch_1.sh', str(run), str(file_dir)])
-    return {"Run": run}
+    run = run + 1 # i+1
+    # Call the appropriate shell file based on the get request
+    sub.call(['sh', f'{file_dir}/touch_{version}.sh', str(run), str(file_dir)])
+    return {"Run": run, "Version": version}
